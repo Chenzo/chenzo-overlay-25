4cb299aa7752809610b66c73cc199f2f68840a8f
Chenzorama <chenzorama@hotmail.com>
2025-04-29 15:46:37 -0400

    whiskey and stuff

--- components/FloatingAlert.js
+++ components/FloatingAlert.js
@@ -6,27 +6,31 @@ import styles from './FloatingAlert.module.scss';
 export default function FloatingAlert({overlayToggle}) {
 
     const [showAFK, setShowAFK] = useState(false);
+    const [videoSRC, setVideoSRC] = useState('');
 
     useEffect(() => {
-        console.log('overlayToggle:', overlayToggle);
+        //console.log('overlayToggle:', overlayToggle);
+
+        setShowAFK(overlayToggle !== "" ? true : false);
 
         if (overlayToggle === "afk") {
-            setShowAFK(true);
-        }  else {
-            setShowAFK(false);
+            setVideoSRC('https://chenzorama.com/overlay/video/hoggle_peeing-small.mp4');
+        } else if (overlayToggle === "whiskey") {
+            setVideoSRC('https://chenzorama.com/overlay/video/whiskey_1.mp4');
         }
+
     }, [overlayToggle]);
 
     if (showAFK) {
         return (
             <article className={styles.floatingAlert}>
                 <div className={styles.border}>
-                    <img src="https://chenzorama.com/overlay/images/ribbons.png" alt="" />
+                    <img src="https://chenzorama.com/overlay/images/ribbons-small.png" alt="" />
     
                 </div>
-                <div className={`${styles.videoHolder} ${styles.torn_3} ${styles.ds}`}>
+                <div className={`${styles.videoHolder} ${styles.torn_3} ${styles.ds}`} key={videoSRC}>
                     <video autoPlay muted loop className={styles.video}>
-                        <source src='https://chenzorama.com/overlay/video/hoggle_peeing.mp4' type='video/mp4' />
+                        <source src={videoSRC} type='video/mp4' />
                     </video>
                 </div>
             </article>
