57026cc6bc1e8e605333799ce4f9e50d3dcca27b
Vince <chenzorama@hotmail.com>
2025-06-15 14:33:09 -0400

    favicon fixes

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,13 @@ import './globals.scss';
 export const metadata = {
   title: `Chenzo's Streaming Overlay '25`,
   description: `Chenzo's Streaming Overlay '25`,
+  icons: {
+    icon: [
+      { url: '/favicon.ico', rel: 'icon', type: 'image/x-icon' },
+      { url: '/favicon-56.png', sizes: '56x56', type: 'image/png' },
+      { url: '/favicon-112.png', sizes: '112x112', type: 'image/png' },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
