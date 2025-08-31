2585d244b52b634f14952f6216c3f48874cd2893
Vince <chenzorama@hotmail.com>
2025-08-31 15:08:40 -0400

    moved everything to S3 bucket

--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,8 @@
 import { useEffect, useRef, useState } from 'react';
 import styles from './Header.module.scss';
 
+const BUCKET_URL = process.env.NEXT_PUBLIC_BUCKET_URL;
+
 export default function Header({ alignment }) {
   const skull = useRef();
   const [alignClass, setAlignClass] = useState(null);
@@ -30,7 +32,7 @@ export default function Header({ alignment }) {
           The Gentlemen of Fortune and the Adventures of The Holy Bartender
         </span>
         <video width='1200' autoPlay={true} muted={true} loop={true}>
-          <source src='https://chenzorama.com/overlay/video/waterup.webm' type='video/webm' />
+          <source src={`${BUCKET_URL}/video/waterup.webm`} type='video/webm' />
         </video>
       </div>
       <div className={styles.alignment}>
